bcd2aa39b3f7a4810d9949bfa893c5a6dce5c0ef
sasakou0911 <sasakou0911@gmail.com>
2019-05-14 14:36:47 +0900

    hello_serial_ver0.0.3

--- hello_serial/hello_serial.py
+++ hello_serial/hello_serial.py
@@ -17,13 +17,13 @@ vehicle = connect(connection_string, wait_ready = True)
 try:
     while True:
         while True:
-        print("-----------------------------")
-        print("Connected now")
-        print("Attitude: %s" % vehicle.attitude)
-        print("velocity: %s" % vehicle.velocity)
-        print("-----------------------------")
-        
-        time.sleep(1)
+            print("-----------------------------")
+            print("Connected now")
+            print("Attitude: %s" % vehicle.attitude)
+            print("velocity: %s" % vehicle.velocity)
+            print("-----------------------------")
+            
+            time.sleep(1)
 
 except( KeyboardInterrupt, SystemExit):
     print("Detect the interruption")
