4e46d3c6b4e135fdcbeb475413da8f690cdcb43b
sasakou0911 <sasakou0911@gmail.com>
2019-05-14 14:19:56 +0900

    hello_serial_ver0.0.1

---
+++ hello_serial/hello_serial.py
@@ -0,0 +1,26 @@
+"""
+Simple script for communicaite with PX4 
+by koichi sasaki
+"""
+
+print("dronekit start now")
+
+from dronekit import connect
+import time
+
+connection_string = "/dev/ttyS0, 57600"
+
+print("connect to FC: %s" % (connection_string))
+
+vehicle = connect(connection_string, wait_ready = True)
+
+try:
+    while True:
+        while True:
+        print("-----------------------------")
+        print("Connected now")
+        print("Attitude: %s" % vehicle.attitude)
+        print("velocity: %s" % vehicle.velocity)
+        print("-----------------------------")
+        
+        time.sleep(1)
